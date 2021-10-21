ada0f00b23b36d34ee9da0173dbe7796573998bb
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2021-10-22 02:39:58 +0900

    [ADD] - Death Counter + Portrait animation

--- SpookyRun/Assets/Scripts/Character/CharacterInventory.cs
+++ SpookyRun/Assets/Scripts/Character/CharacterInventory.cs
@@ -2,29 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CharacterInventory : MonoBehaviour
 {
-    public Text boxCount;
+    private Rigidbody2D rigidBody2D;
+
+    public Animator portraitAnimation;
+
+    public Text boxCountLabel;
+    public Text deathCountLabel;
+
+    static int boxCount = 0;
+    static int deathCount = 0;
+
+	void Awake()
+	{
+		rigidBody2D = GetComponent<Rigidbody2D>();
+	}
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.SetInt("boxCount", 0);
-        boxCount.text = ": " + PlayerPrefs.GetInt("boxCount");
-        PlayerPrefs.SetInt("deathCount", 0);
+        boxCountLabel.text = ": " + boxCount;
+        deathCountLabel.text = ": " + deathCount;
     }
 
     void UpdateBoxCount()
     {
-        PlayerPrefs.SetInt("boxCount", PlayerPrefs.GetInt("boxCount") + 1);
-        boxCount.text = ": " + PlayerPrefs.GetInt("boxCount");
+        boxCount += 1;
+        boxCountLabel.text = ": " + boxCount;
+    }
+
+    void UpdateDeathCount()
+    {
+        deathCount += 1;
+        deathCountLabel.text = ": " + deathCount;
+        // TRIGGER ANIMATION
+        portraitAnimation.SetBool("isDamaged", true);
+    }
+
+    void ResetDamage()
+    {
+        portraitAnimation.SetBool("isDamaged", false);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Box") {
             UpdateBoxCount();
+        } else if (collision.gameObject.tag == "DeathDrop") {
+            UpdateDeathCount();
+            rigidBody2D.position = new Vector2(-4, 7);
         }
     }
 }
