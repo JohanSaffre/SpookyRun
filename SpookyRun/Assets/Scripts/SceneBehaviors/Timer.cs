f73981de1df46d61df30d4c490e0e1362abde589
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2021-10-22 01:20:23 +0900

    [ADD] - Countdown timer

---
+++ SpookyRun/Assets/Scripts/SceneBehaviors/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeValue = 300;
+    public Text timerLabel;
+
+    private void DisplayTimer()
+    {
+        int roungTimeValue = (int)timeValue;
+
+        timerLabel.text = roungTimeValue.ToString().PadLeft(3, '0');
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerLabel = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeValue >= 0) {
+            timeValue -= Time.deltaTime;
+            DisplayTimer();
+        }
+    }
+}
