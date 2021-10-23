f3e8fc9634e87d62c1bff4399af15f6576ebed5b
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2021-10-23 18:22:37 +0900

    [ADD] - AudioManager

---
+++ SpookyRun/Assets/Scripts/AudioManager/AudioManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioManager instance;
+
+    public Sound[] sounds;
+
+    // Awake is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound sound in sounds) {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+
+            sound.source.loop = sound.loop;
+        }
+        
+    }
+
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    // Update is called once per frame
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null) {
+            Debug.LogWarning("Sound: \"" + name +"\": not found.");
+            return;
+        }
+        s.source.Play();
+    }
+}
--- SpookyRun/Assets/Scripts/Character/CharacterInventory.cs
+++ SpookyRun/Assets/Scripts/Character/CharacterInventory.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class CharacterInventory : MonoBehaviour
 {
     private Rigidbody2D rigidBody2D;
-
+    public AudioManager audioManager;
     public Animator portraitAnimation;
 
     public Text boxCountLabel;
@@ -32,6 +32,8 @@ public class CharacterInventory : MonoBehaviour
     {
         boxCount += 1;
         boxCountLabel.text = ": " + boxCount;
+        // PLAY SOUNDS
+        audioManager.Play("CollectBox");
     }
 
     void UpdateDeathCount()
