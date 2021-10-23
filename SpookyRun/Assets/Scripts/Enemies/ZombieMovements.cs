f7f0dfb6a77459496cd610dca403d6529c4f729f
Johan Saffre <49069697+JohanSaffre@users.noreply.github.com>
2021-10-23 21:37:33 +0900

    [add] - Zombies animations, movements & placement

---
+++ SpookyRun/Assets/Scripts/Enemies/ZombieMovements.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovements : MonoBehaviour
+{
+    private BoxCollider2D _box;
+    private Transform _transform;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    private string direction = "right";
+    public float speed = 1;
+
+    void Start()
+    {
+        _box = GetComponent<BoxCollider2D>();
+        _transform = GetComponent<Transform>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+        _animator.SetTrigger("Walking");
+    }
+
+    void Update()
+    {
+        if (direction == "right") {
+            _transform.position += new Vector3(1 * Time.deltaTime * speed, 0, 0);
+        }
+        if (direction == "left") {
+            _transform.position -= new Vector3(1 * Time.deltaTime * speed, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D box) {
+        if (box.tag == "ZombiBoxLeft") {
+            direction = "right";
+            _spriteRenderer.flipX = false;
+        }
+        if (box.tag == "ZombiBoxRight") {
+            direction = "left";
+            _spriteRenderer.flipX = true;
+        }
+    }
+}
